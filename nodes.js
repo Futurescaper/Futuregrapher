555dedc7aa05f461f8add4af7b74aba3047f07c0
kevinmarzec@gmail.com <g0t0he11>
2013-09-25 15:45:02 +0100

    Package creation

--- nodes.js
+++ nodes.js
@@ -8,7 +8,7 @@ d3nodes = function (graph) {
             return d3colors.rgba(colors.getRgbaFromHex(node._color));
 
         var color = d3colors.blend(d3colors.getRgbaFromHex(minColor || graph.d3styles().colors.nodeMin), d3colors.getRgbaFromHex(maxColor || graph.d3styles().colors.nodeMax), node.ratio);
-        var fill = d3colors.rgba(color);
+        var fill = color.rgbastr();
         if (node.color != fill)
             node.color = fill;
 
