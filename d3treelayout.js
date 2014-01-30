d9c368f32da4a53c0c9a5a43c69ba218980a1cf3
kevinmarzec@gmail.com <g0t0he11>
2014-01-30 11:24:06 +0000

    treeview.saveState fix

--- d3treelayout.js
+++ d3treelayout.js
@@ -30,41 +30,41 @@ if(Meteor.isClient) {
 
         function d3treelayout(el, options) {
             this.el = el;
-        
-            var m = options.margins; 
-                w = el.width() - m[1] - m[3],
+
+            var m = options.margins;
+            w = el.width() - m[1] - m[3],
                 h = el.height() - m[0] - m[2];
-        
+
             this.width = w;
             this.height = h;
-        
+
             var vis = d3.select('#' + this.el.attr('id')).append("svg:svg")
                 .attr("width", w + m[1] + m[3])
                 .attr("height", h + m[0] + m[2])
                 .append("svg:g")
                 .attr("transform", "translate(" + m[3] + "," + m[0] + ")");
-        
+
             var axisLayer = vis.append("svg:g");
-        
+
             var tree = d3.layout.tree()
                 .size([h, w]);
-        
+
             var diagonal = d3.svg.diagonal()
                 .projection(function(d) { return [d.y, d.x]; });
-        
+
             if(options.hideAxis)
                 axisLayer.style("opacity", "0");
-        
+
             function createAxis(domain) {
                 var scale = d3.scale.ordinal()
                     .domain(domain)
                     .rangePoints([0, w]);
-            
+
                 var axis = d3.svg.axis()
                     .scale(scale)
                     .orient("bottom")
                     .tickSize(-h - 80);
-            
+
                 if(domain.length > 20) {
                     var tickValues = [];
                     var tick = 0;
@@ -75,29 +75,30 @@ if(Meteor.isClient) {
                     }
                     axis.tickValues(tickValues);
                 }
-            
+
                 axisLayer.append("svg:g")
                     .attr("class", "timeline-axis")
                     .attr("transform", "translate(0, " + (h + 80) + ")")
                     .call(axis);
             }
-        
+
             // If we're hiding the root node, level 1 is the left-most
             var shallowestLevel = options.hideRootNode ? 1 : 0;
-        
+
             var root;
-        
+
             this.create = function (treeData, domain) {
                 this.treeData = treeData;
-            
+                this.domain = domain;
+
                 root = treeData;
                 root.x0 = h / 2;
                 root.y0 = 0;
-            
+
                 function initializeData() {
                     var idCounter = 0;
                     root.id = idCounter++;
-                
+
                     function initializeBranch(node) {
                         if (node.children) {
                             node.children.forEach(initializeBranch);
@@ -106,35 +107,35 @@ if(Meteor.isClient) {
                         node.id = idCounter++;
                         node.selected = false;
                     }
-                
+
                     root.children.forEach(initializeBranch);
                 }
                 initializeData();
-            
+
                 createAxis(domain);
             }
             this.loadFromState = function (savedState) {
                 this.treeData = savedState.treeData;
                 root = this.treeData;
-            
+
                 createAxis(savedState.domain);
             }
             this.saveState = function () {
                 return {
                     options: options,
-                    treeData: treeData,
-                    domain: domain
-                };        
+                    treeData: this.treeData,
+                    domain: this.domain
+                };
             }
-                
+
             this.update = function () {
                 update();
             }
-        
+
             this.setOption = function (key, value) {
                 options[key] = value;
             }
-        
+
             this.showAxis = function() {
                 options.hideAxis = false;
                 axisLayer.transition(500).style("opacity", "1");
@@ -143,84 +144,84 @@ if(Meteor.isClient) {
                 options.hideAxis = true;
                 axisLayer.transition(500).style("opacity", "0");
             }
-        
+
             function updateNodePositions(nodeToExclude) {
                 var node = vis.selectAll("g.timeline-node");
-            
+
                 if(nodeToExclude)
                     node = node.filter(function (d) { return d !== nodeToExclude; });
-                
+
                 node.transition(500)
                     .attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; });
-            
+
                 var linkSelector = "path.timeline-link";
-                
+
                 if(nodeToExclude)   // Exclude links to and from our node...
                     linkSelector += ":not([data-link-source='" + nodeToExclude.id + "']):not([data-link-target='" + nodeToExclude.id + "'])";
-            
+
                 var link = d3.selectAll(linkSelector)
                     .transition(500)
                     .attr("d", diagonal);
             }
-        
+
             function dragStart(d) {
                 if(!options.enableDragging)
                     return;
-            
+
                 if(options.dragStartHandler) {
                     var nodeElement = d3.select(this);
                     var node = nodeElement.datum();
                     options.dragStartHandler(node, nodeElement);
                 }
             }
-            
+
             function dragMove(d) {
                 if(!options.enableDragging)
                     return;
-            
+
                 var nodeElement = d3.select(this);
                 var node = nodeElement.datum();
-            
+
                 var newX = d3.event.x;
                 var newY = d3.event.y;
-                
+
                 var oldX = node.y;
                 var oldY = node.x;
-            
+
                 if (options.dragMoveHandler) {
                     var result = options.dragMoveHandler(node, nodeElement, newX, newY, oldX, oldY);
                     newX = result.x;
                     newY = result.y;
-                    
+
                     if(result.update)
                         updateNodePositions(node);
                 }
-            
+
                 node.y = newX;
                 node.x = newY;
-            
+
                 nodeElement.attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; });
-            
+
                 var link = d3.selectAll("[data-link-source='" + node.id + "'], [data-link-target='" + node.id + "']");
                 link.attr("d", diagonal);
             }
-            
+
             function dragEnd(d) {
                 if(!options.enableDragging)
                     return;
-                    
+
                 var nodeElement = d3.select(this);
                 var node = nodeElement.datum();
-            
+
                 if (options.dragEndHandler) {
                     var finalCoords = options.dragEndHandler(node, nodeElement);
                     node.y = finalCoords[0];
                     node.x = finalCoords[1];
                 }
-            
+
                 updateNodePositions();
             }
-            
+
             var drag = d3.behavior.drag()
                 .origin(function() {
                     var d = d3.select(this).datum();
@@ -229,11 +230,11 @@ if(Meteor.isClient) {
                 .on("dragstart", dragStart)
                 .on("drag", dragMove)
                 .on("dragend", dragEnd);
-        
+
             // Toggle children of a given node
             function toggle(node) {
                 node.selected = !node.selected;
-            
+
                 if (node.children) {
                     node._children = node.children;
                     node.children = null;
@@ -242,25 +243,25 @@ if(Meteor.isClient) {
                     node._children = null;
                 }
             }
-            
+
             function update(source) {
                 source = source || root;
                 var duration = 500;
-            
+
                 // Compute the new tree layout.
                 this.nodes = tree.nodes(root).reverse();
                 if(options.hideRootNode) {
                     this.nodes = _(this.nodes).filter(function (n) { return n.id !== root.id; });
                 }
-            
+
                 // If a normalize position-function was given, apply it to every node
                 if (options.normalizePositions)
                     options.normalizePositions();
-            
+
                 // Update the nodes
                 var node = vis.selectAll("g." + options.nodeClass)
                     .data(this.nodes, function(d) { return d.id; })
-            
+
                 // Enter any new nodes at the parent's previous position.
                 var nodeEnter = node.enter().append("svg:g")
                     .attr("class", function (d) { return options.nodeClass + " " + (d.selected ? options.nodeClassSelected : options.nodeClassUnselected); })
@@ -275,14 +276,14 @@ if(Meteor.isClient) {
                         }
                     })
                     .call(drag);
-            
+
                 nodeEnter.append("svg:circle")
                     .attr("r", 1e-6)
                     .style("fill", function(d) { return d._children ? options.nodeColorUnexpanded : options.nodeColorExpanded; });
-            
+
                 // Switch between -1 and 1
                 function alternate(row) { return (row % 2) * 2 - 1; }
-            
+
                 nodeEnter.append("svg:text")
                     .attr("x", function(d) { return d.children || d._children || d.depth === shallowestLevel ? -10 : 10; })
                     .attr("y", function(d) {
@@ -292,39 +293,39 @@ if(Meteor.isClient) {
                     .attr("text-anchor", function(d) { return d.children || d._children || d.depth === shallowestLevel ? "end" : "start"; })
                     .text(function(d) { return Helper.getWords(d.name, 5); })
                     .style("fill-opacity", 1e-6);
-            
+
                 nodeEnter.append("svg:title")
                     .text(function (d) { return d.name; });
-            
+
                 // Transition nodes to their new position.
                 var nodeUpdate = node.transition()
                     .duration(duration)
                     .attr("class", function (d) { return options.nodeClass + " " + (d.selected ? options.nodeClassSelected : options.nodeClassUnselected); })
                     .attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; })
-            
+
                 nodeUpdate.select("circle")
                     .attr("r", 4.5)
                     .style("fill", function(d) { return d._children ? options.nodeColorUnexpanded : options.nodeColorExpanded; });
-            
+
                 nodeUpdate.select("text")
                     .style("fill-opacity", 1);
-            
+
                 // Transition exiting nodes to the parent's new position.
                 var nodeExit = node.exit().transition()
                     .duration(duration)
                     .attr("transform", function(d) { return "translate(" + source.y + "," + source.x + ")"; })
                     .remove();
-            
+
                 nodeExit.select("circle")
                     .attr("r", 1e-6);
-            
+
                 nodeExit.select("text")
                     .style("fill-opacity", 1e-6);
-            
+
                 // Update the links
                 var link = vis.selectAll("path." + options.linkClass)
                     .data(tree.links(nodes), function(d) { return d.target.id; });
-            
+
                 // Enter any new links at the parent's previous position.
                 link.enter().insert("svg:path", "g")
                     .attr("class", function (d) { return options.linkClass + " " + (d.target.selected ? options.linkClassSelected : options.linkClassUnselected); })
@@ -337,13 +338,13 @@ if(Meteor.isClient) {
                     .transition()
                     .duration(duration)
                     .attr("d", diagonal);
-            
+
                 // Transition links to their new position.
                 link.transition()
                     .attr("class", function (d) { return options.linkClass + " " + (d.target.selected ? options.linkClassSelected : options.linkClassUnselected); })
                     .duration(duration)
                     .attr("d", diagonal);
-            
+
                 // Transition exiting nodes to the parent's new position.
                 link.exit().transition()
                     .duration(duration)
@@ -352,7 +353,7 @@ if(Meteor.isClient) {
                         return diagonal({source: o, target: o});
                     })
                     .remove();
-            
+
                 // Stash the old positions for transition.
                 nodes.forEach(function(d) {
                     d.x0 = d.x;
