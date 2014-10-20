215be0219a905cb544abf4c7c4f268e4da65a776
kevinmarzec@gmail.com <kevinmarzec@gmail.com>
2014-10-20 11:57:30 +0100

    fixing grunt

--- dist/futuregrapher.js
+++ dist/futuregrapher.js
@@ -4431,37 +4431,37 @@ define('futuregrapher/gradientscale',['require'],function(require) {
             range = ["#333", "#aaa"],
             domainScale = d3.scale.linear(),
             rangeScale = d3.scale.linear().range(range);
-    
+
         function rescale() {
             domainScale
                 .domain(domain)
                 .range([0, range.length - 1]);
-                
+
             rangeScale
                 .domain(d3.range(0, range.length))
                 .range(range);
-            
+
             return scale;
         }
-        
+
         function scale(x) {
             return rangeScale(domainScale(x));
         }
-        
+
         scale.domain = function (x) {
             if (!arguments.length) return domain;
             domain = x;
             return rescale();
         };
-        
+
         scale.range = function (x) {
             if (!arguments.length) return range;
             range = x;
             return rescale();
         };
-        
+
         return rescale();
-    }
+    };
 
     return gradientScale;
 });
